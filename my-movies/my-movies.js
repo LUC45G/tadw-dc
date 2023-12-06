f81e0e78a478ec0105caa7abb55d4a41e8d5984d
matichewer <schwerdt.matias@gmail.com>
2023-12-06 11:52:33 -0300

    my-movies: Dockerfile and docker-compose

--- my-movies/my-movies.js
+++ my-movies/my-movies.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const Movie = require('./movieSchema');
+const mongoose = require("mongoose");
+const Movie = require("./movieSchema");
 
-app.get('/', async (req, res) => {
-    try {
-        // Obtén la lista de películas desde la base de datos MongoDB
-        const myMovies = await Movie.find();
+app.get("/", async (req, res) => {
+  try {
+    // Obtén la lista de películas desde la base de datos MongoDB
+    const myMovies = await Movie.find();
 
-        // Envia la lista de películas como respuesta
-        res.json(myMovies);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error interno del servidor');
-    }
+    // Envia la lista de películas como respuesta
+    res.json(myMovies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 const PORT = 3003;
 
-mongoose.connect('mongodb://localhost:27017/movies', { // TODO cambiar esta direccion
-}).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => {
-    console.log(`MyMovies microservice is running on port ${PORT}`);
-  });
-}).catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose
+  .connect("mongodb://admin:admin@mongo:27017/movies?authSource=admin")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`MyMovies microservice is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
